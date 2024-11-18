fd9ed9772f278799f7ec2b18b617c3fbd86fc08f
unknown <a.cina@procreatech.com>
2024-11-18 13:07:47 +0100

    8. How to setup Lazy Loading with the Angular Router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,22 @@ import {
 } from "@angular/router";
 import { LoginComponent } from "./login/login.component";
 import { AboutComponent } from "./about/about.component";
+import { CoursesModule } from "./courses/courses.module";
 
+// questa app contiene un modulo separato relativo ai courses, questo contiene services, componenti, un suo router etc
+// questo modulo vogliamo che sia caricato in lazy loading, cioè che venga caricato solo quando se ne ha realemnte bisogno, solo quando accediamo in qualche parte di app che ne ha bisogno
+// la prima cosa da fre per attivare in maniera pigra un modulo tramite router è definire l'activation path del router. Nel caso del modulo courses questo deve essere attivato solo quando si ha una rotta che inizia con /courses
 const routes: Routes = [
+  {
+    // definisco il path per l'attivazione del modulo courses
+    path: "courses",
+    // gli import che utilizzo all'inizio di un file sono import statici, ad es. import { NgModule } from "@angular/core";
+    // per essere caricato in maniera lazy, il modulo viene chiamato in modo asincrono, non utilizzo la proprietà component ma loadChidren che contiene al suo interno tutte le rotte figlie che iniziano con /courses
+    // ha come valore una funzione che ritorna il modulo, cioè importiamo il modulo dinamicamente utilizzando vanilla js, il metodo import() al quale passiamo il path di quello che vogliamo importare
+    // questo ci restituisce una promise, tramite .then accedo al file ed indico cosa devo esportare
+    loadChildren: () =>
+      import("./courses/courses.module").then((m) => m.CoursesModule),
+  },
   {
     path: "login",
     component: LoginComponent,

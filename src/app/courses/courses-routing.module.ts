459cbc96cc921faf370ffb9c6a7b6f6ad0251b54
unknown <a.cina@procreatech.com>
2024-11-18 14:48:03 +0100

    9. Angular Router Lazy Loaded Modules - Runtime Demo

--- src/app/courses/courses-routing.module.ts
+++ src/app/courses/courses-routing.module.ts
@@ -1,22 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
-
+  // la prima rotta che aggiungiamo qui è quella della root, quindi quella che corrisponderebbe a localhost:4200/courses, lasceremo il path vuoto
+  {
+    path: "",
+    component: HomeComponent,
+  },
 ];
 
 @NgModule({
-  imports: [
-
-  ],
+  // importiamo il RouterModule ed utilizziamo il metodo forChild(), diciamo quindi che queste rotte sono le child di quella indicata in app-routing.module.ts con path "courses" ed in cui abbiamo importato in lazy loading il modulo CoursesModule
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
-  providers: [
-
-  ]
+  providers: [],
 })
-export class CoursesRoutingModule {
-
-
-
-}
+export class CoursesRoutingModule {}
